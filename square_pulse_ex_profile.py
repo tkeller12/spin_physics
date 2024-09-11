7cc8c2e0949174cd91aa2bdb6ed839b188b32482
Timothy Keller <tkeller@bridge12.com>
2024-09-11 14:00:07 -0400

    Updated rectangular excitation profile simulation

--- square_pulse_ex_profile.py
+++ square_pulse_ex_profile.py
@@ -20,87 +20,52 @@ sigma_z = 0.5*np.r_[
             [0, -1] ]
         ] + 0j
 
+print('Density Operators:')
 print(sigma_x)
 print(sigma_y)
 print(sigma_z)
-
-
-print('commutator test')
-print(np.dot(sigma_x,sigma_y) - np.dot(sigma_y,sigma_x))
-print(sigma_z * 1j)
+print('')
+print('Pass Commutator Test:')
 print(np.allclose(sigma_z * 1j, np.dot(sigma_x,sigma_y) - np.dot(sigma_y,sigma_x)))
+print('')
 
-omega = 100000. # frequency offset from carrier, Hz
-#tp = 0.0001 # Pulse Length, s
-B1 = 20000
+omega = 500e6 # Bandwidth of Simulation (offset from carrier, Hz)
+tp = 20e-9 # Pulse Length, s
 pts = 1024 # Points in FID
 
-tp = np.pi/2 / B1
+pulse_angle = np.pi # pi/2 for 90-pulse, pi for 180-pulse
+
+B1 = pulse_angle / tp
 
 omega_array = np.r_[-omega/2:omega/2:1j*pts]
 
 coil = sigma_x + 1j*sigma_y # Detection Operator (NMR Coil)
 
-#H = 2*np.pi * omega * sigma_z # Calculate Hamiltonian (only Zeeman)
-
-#P = expm(1j*H*dt) # Define Propagator
-
-#sigma = sigma_z # Initial Density Matrix (After 90-pulse)
-
 M_list = []
+Mz_list = []
 
 for ix in range(pts):
     sigma = sigma_z # Initial Density Matrix
 
-    sigma = np.r_[[[1,0], [0,0]]]
     # re-calculate spin hamiltonian for offset
-#    H = 2*np.pi * omega_array[ix] * sigma_z + np.pi/2 * sigma_y # Calculate Hamiltonian (only Zeeman)
-
     H = 2*np.pi * tp * omega_array[ix] * sigma_z + B1 * tp * sigma_x # Calculate Hamiltonian (only Zeeman)
-#    H = 2*np.pi * tp * omega_array[ix] * sigma_z + B1 * tp * sigma_y # Calculate Hamiltonian (only Zeeman)
     P = expm(1j*H) # Define Propagator
-#    sigma = np.dot(np.dot(P,sigma),np.conjugate(P)) # Propagate Density Matrix
     sigma = np.dot(np.dot(P,sigma),P.T.conj()) # Propagate Density Matrix
 
-#    sigma = P @ sigma @ P.T.conj()
-
     M = np.trace(np.dot(coil,sigma)) # Detect
+    Mz = np.trace(np.dot(sigma_z,sigma))
     M_list.append(M) # Append to FID array
+    Mz_list.append(Mz)
 
 
-M = np.array(M_list)
+M_array = np.array(M_list)
+Mz_array = np.array(Mz_list)
 
 figure('Excitation Profile')
 title('Excitation Profile')
-plot(omega_array, np.real(M), label = 'real')
-plot(omega_array, np.imag(M), label = 'imag')
-plot(omega_array, np.abs(M), label = 'abs')
+plot(omega_array/1e6, np.real(M_array), label = 'Mx')
+plot(omega_array/1e6, np.imag(M_array), label = 'My')
+plot(omega_array/1e6, np.real(Mz_array), label = 'Mz')
 legend()
-xlabel('Frequency')
-
-#figure('spec')
-#title('Spectrum')
-#plot(f,np.real(spec))
-#xlabel('Frequency (Hz)')
-
-Px = expm(1j*(np.pi/2)*sigma_x)
-Py = expm(1j*(np.pi/2)*sigma_y)
-
-#sigma_init = np.r_[
-#        [
-#            [1,0], 
-#            [0,0]]
-#        ]
-
-sigma_init = sigma_z
-#sigma_init = np.r_[[[1,0], [0,0]]]
-#testx = Px @ sigma_init @ Px.conj()
-#testy = Py @ sigma_init @ Py.conj()
-testx = Px @ sigma_init @ Px.T # x-pulse does not work
-testy = Py @ sigma_init @ Py.T
-
-
-print(testx)
-print(testy)
-
+xlabel('Frequency Offset (MHz)')
 show()
