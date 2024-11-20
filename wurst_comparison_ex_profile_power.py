11d2080bd5c081d703b183d02e7a50bf0a7d6688
Timothy Keller <tkeller@bridge12.com>
2024-11-20 16:40:03 -0500

    Updating WURST pulse simulation

---
+++ wurst_comparison_ex_profile_power.py
@@ -0,0 +1,127 @@
+import numpy as np
+from scipy.linalg import expm
+from matplotlib.pylab import *
+
+def wurst(tp, N, resolution):
+    '''Real value WURST envelope pulse shape
+
+    .. math::
+        1 - \\text{abs} \left( \cos \left( \\frac{\pi}{t_p} (t - \\frac{t_p}{2}) + \\frac{\pi}{2} \\right) \\right) ^N
+
+    Args:
+        tp (float): Pulse length
+        N (float): exponential 
+
+    Returns:
+        tuple: tuple containing:
+
+        t (*numpy.ndarray*): Time axis
+
+        pulse (*numpy.ndarray*): Pulse shape
+    '''
+    t = np.r_[0.:tp:resolution]
+    pulse = (1. - np.abs(np.cos(np.pi*(t-tp/2.)/tp + np.pi/2.))**N) + 0j
+
+    return t, pulse
+
+sigma_x = 0.5*np.r_[
+        [
+            [0, 1], 
+            [1, 0] ]
+        ] + 0j
+
+sigma_y = 0.5*np.r_[
+        [
+            [0,-1j], 
+            [1j, 0] ]
+        ] + 0j
+
+sigma_z = 0.5*np.r_[
+        [
+            [1, 0], 
+            [0, -1] ]
+        ] + 0j
+
+print('Sx: ', sigma_x)
+print('Sy: ', sigma_y)
+print('Sz: ', sigma_z)
+
+
+print('Test Commutator:')
+print(np.dot(sigma_x,sigma_y) - np.dot(sigma_y,sigma_x))
+print(sigma_z * 1j)
+print(np.allclose(sigma_z * 1j, np.dot(sigma_x,sigma_y) - np.dot(sigma_y,sigma_x)))
+
+omega_bw = 500e6 # frequency offset from carrier, Hz
+
+tp_180 = 20e-9# 180-pulse length, s
+pts = 128 # Points in FID
+
+pulse_B1 = np.pi / tp_180
+print('B1 power: %0.03e Angular Frequency'%pulse_B1)
+
+omega_array = np.r_[-omega_bw/2:omega_bw/2:1j*pts]
+
+coil = sigma_x + 1j*sigma_y # Detection Operator (NMR Coil)
+
+for power in [2, 5, 10, 100]:
+    print('Power: ', power)
+
+    tp = 20e-9
+    BW = 400e6
+    dt = 0.1e-9
+    amp = 1.
+    t, shape = wurst(tp, power, dt)
+    pulse = pulse_B1 * amp * shape
+
+    figure('Pulse Shape')
+    plot(t*1e9,np.real(pulse)/pulse_B1, label = 'WURST-%i'%power)
+    xlabel('Time (ns)')
+    ylabel('Normalized Pulse Amplitude')
+    legend()
+    xlim(np.min(t*1e9), np.max(t*1e9))
+    tight_layout()
+
+    M_list = []
+    Mz_list = []
+
+    Mz_array = np.zeros((len(t),len(omega_array)))
+
+    for omega_ix,omega in enumerate(omega_array):
+        sigma = sigma_z # Initial Density Matrix
+
+        for time_ix,time in enumerate(t):
+            B1 = pulse[time_ix]
+            # re-calculate spin hamiltonian for offset
+            H = 2*np.pi * omega * sigma_z + np.real(B1) * sigma_x + np.imag(B1) * sigma_y # Calculate Hamiltonian
+            P = expm(1j*H*dt) # Define Propagator
+            sigma = np.dot(np.dot(P,sigma),P.T.conj()) # Propagate Density Matrix
+
+            Mz_value = 2*np.real(np.trace(np.dot(sigma_z,sigma)))
+            Mz_array[time_ix, omega_ix] = Mz_value
+
+        M = 2*np.trace(np.dot(coil,sigma)) # Detect Sx & Sy
+    #    Mz_value = 2*np.trace(np.dot(sigma_z,sigma)) # Detect Sz
+        M_list.append(M)
+        Mz_list.append(Mz_value)
+
+    M = np.array(M_list)
+    Mz = np.array(Mz_list)
+
+    #figure('Excitation_Profile_WURST-%i_%0.0fns_%0.0fMHz'%(power,tp*1e9,BW/1e6))
+    #title('Excitation Profile\nWURST-%i, %0.0f ns, %0.0f MHz'%(power,tp*1e9,BW/1e6))
+    figure('Excitation_Profile')
+    title('Excitation Profile')
+    #plot(omega_array/1e6, np.real(M), label = 'Mx')
+    #plot(omega_array/1e6, np.imag(M), label = 'My')
+    plot(omega_array/1e6, np.real(Mz), label = 'WURST-%i'%power)
+    xlim(np.min(omega_array/1e6), np.max(omega_array/1e6))
+    legend()
+    xlabel('Frequency (MHz)')
+    ylabel('Mz')
+    tight_layout()
+
+    #figure()
+    #imshow(np.real(Mz_array), aspect = 'auto')
+    #colorbar()
+show()
--- wurst_ex_profile.py
+++ wurst_ex_profile.py
@@ -35,9 +35,7 @@ print(np.allclose(sigma_z * 1j, np.dot(sigma_x,sigma_y) - np.dot(sigma_y,sigma_x
 omega_bw = 500e6 # frequency offset from carrier, Hz
 #tp = 0.0001 # Pulse Length, s
 
-tp_180 = 20e-9# 180-pulse length, s
-#B1 = 60e6
-#B1 = 0e6
+tp_180 = 10e-9# 180-pulse length, s
 pts = 128 # Points in FID
 
 #tp = np.pi/2 / B1
@@ -49,29 +47,24 @@ omega_array = np.r_[-omega_bw/2:omega_bw/2:1j*pts]
 coil = sigma_x + 1j*sigma_y # Detection Operator (NMR Coil)
 
 
-tp = 20e-9
-BW = 400e6
-dt = 0.1e-9
-amp = 1.
-power = 100. # 
+tp = 128e-9
+BW = 100e6
+dt = 0.5e-9
+amp = 0.3
+power = 5. # 
 t,shape = deer.wurst(tp, power, resolution = dt)
 #t,shape = deer.adiabatic(tp, BW, 3, resolution = dt)
 #t, shape = deer.sinc(tp, 10, resolution = dt)
 t, chirp = deer.chirp(tp, BW, resolution = dt)
 
-#pulse = amp * shape * chirp
+pulse = amp * shape * chirp
 #pulse = amp * chirp
-pulse = amp * shape
+#pulse = amp * shape
 
 pulse *= B1
 figure('Pulse Shape')
 plot(t,np.real(pulse))
 plot(t,np.imag(pulse))
-#H = 2*np.pi * omega * sigma_z # Calculate Hamiltonian (only Zeeman)
-
-#P = expm(1j*H*dt) # Define Propagator
-
-#sigma = sigma_z # Initial Density Matrix (After 90-pulse)
 
 M_list = []
 Mz_list = []
@@ -79,11 +72,11 @@ Mz_list = []
 Mz_array = np.zeros((len(t),len(omega_array)))
 
 for omega_ix,omega in enumerate(omega_array):
-#    sigma = sigma_z # Initial Density Matrix
+    print('%i of %i'%(omega_ix,len(omega_array)))
+    sigma = sigma_z # Initial Density Matrix
 
-    sigma = np.r_[[[1,0], [0,0]]] + np.eye(2)
+#    sigma = np.r_[[[1,0], [0,0]]] + np.eye(2)
     # re-calculate spin hamiltonian for offset
-#    H = 2*np.pi * omega_array[ix] * sigma_z + np.pi/2 * sigma_y # Calculate Hamiltonian (only Zeeman)
 
     for time_ix,time in enumerate(t):
         B1 = pulse[time_ix]
@@ -93,7 +86,6 @@ for omega_ix,omega in enumerate(omega_array):
 
         Mz_value = np.real(np.trace(np.dot(sigma_z,sigma)))
         Mz_array[time_ix, omega_ix] = Mz_value
-#    sigma = P @ sigma @ P.T.conj()
 
     M = 2*np.trace(np.dot(coil,sigma)) # Detect
     Mz_value = 2*np.trace(np.dot(sigma_z,sigma))
@@ -112,34 +104,8 @@ plot(omega_array/1e6, np.real(Mz), label = 'Mz')
 legend()
 xlabel('Frequency (MHz)')
 
-#figure('spec')
-#title('Spectrum')
-#plot(f,np.real(spec))
-#xlabel('Frequency (Hz)')
-
-Px = expm(1j*(np.pi/2)*sigma_x)
-Py = expm(1j*(np.pi/2)*sigma_y)
-
-#sigma_init = np.r_[
-#        [
-#            [1,0], 
-#            [0,0]]
-#        ]
-
-sigma_init = sigma_z
-#sigma_init = np.r_[[[1,0], [0,0]]]
-#testx = Px @ sigma_init @ Px.conj()
-#testy = Py @ sigma_init @ Py.conj()
-testx = Px @ sigma_init @ Px.T # x-pulse does not work
-testy = Py @ sigma_init @ Py.T
-
-
-print(testx)
-print(testy)
-
 figure()
 imshow(np.real(Mz_array), aspect = 'auto')
 colorbar()
-
 
 show()
